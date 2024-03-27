89f0908c3fa94db0fb5293f307cae7103e112216
unknown <alm1427@hotmail.com>
2024-03-26 22:33:27 -0300

    PawMate UI

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {Text, Image, View, StyleSheet, ImageBackground} from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  <View style={styles.pageContainer}>
+    <View style={styles.card}>
+      <ImageBackground
+        source={{
+         uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png'
+        }}
+        style={styles.image}>
+        <View style={styles.cardInner}>
+          <Text style={styles.name}>Elon Musk</Text>
+          <Text style={styles.bio}>
+            loren kdsn kfwlfkmwe  kmwgkl mfwkfkf wkl wlefkwfmwelf
+          </Text>
+        </View>
+      </ImageBackground>
     </View>
+  </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  pageContainer: {
     justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  card: {
+    width: '95%',
+    height: '78%',
+    borderRadius: 10,
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 8,
+      height: 5,
+    },
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+
+    elevation: 11,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 18,
+    overflow: 'hidden',
+
+    justifyContent: 'flex-end',
+  },
+  cardInner: {
+    padding: 10,
+  },
+  name: {
+    fontSize: 38,
+    color: 'white',
+    fontWeight: 'bold',
+    marginHorizontal: 18,
+  },
+  bio: {
+    fontSize: 18,
+    color: 'white',
+    lineHeight: 25,
+    marginHorizontal: 10,
   },
 });
+
+export default App;
