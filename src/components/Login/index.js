f8000f50ef1245a49727e39bd9b6c9d1b1472907
Ragazzi147 <91346278+Ragazzi147@users.noreply.github.com>
2024-03-28 13:26:13 -0300

    background e logo

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Text, TextInput, TouchableOpacity, Platform } from "react-native";
+import { View, StyleSheet, Text, TextInput, TouchableOpacity, Image } from "react-native"; // Importe o componente Image
+
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -17,70 +19,92 @@ export default function Login() {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        onChangeText={setEmail}
-        value={email}
-        keyboardType="email-address"
-        autoCapitalize="none"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        onChangeText={setPassword}
-        value={password}
-        secureTextEntry
-      />
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
-        <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={handleSignUp}>
-        <Text style={styles.signUpText}>Não tem cadastro? Cadastre-se aqui</Text>
-      </TouchableOpacity>
-    </View>
+    <LinearGradient
+      colors={['#4B768A', '#06285B']}
+      style={styles.container}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+    >
+      <View style={styles.overlay}>
+        {/* Adicione o componente Image para a sua logo */}
+        <Image source={require('../../../assets/OIG4.png')} style={styles.logo} />
+        
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={setEmail}
+          value={email}
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={setPassword}
+          value={password}
+          secureTextEntry
+        />
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleSignUp}>
+          <Text style={styles.signUpText}>Não tem cadastro? Cadastre-se aqui</Text>
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+   width: "100%",
+   height: "100%",
+  },
+  overlay: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     paddingHorizontal: 20,
-    paddingTop: Platform.OS === 'android' ? 25 : 0, // Adiciona padding para Android
   },
+  logo: {
+    width: 300, // Ajuste conforme desejado
+    height: 300, // Ajuste conforme desejado
+    resizeMode: 'contain', // Mantém a proporção da imagem
+
+  },
+  
   title: {
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 20,
+    color: "white",
   },
   input: {
-    width: 200,
+    width: "100%",
     height: 40,
     borderColor: "gray",
     borderWidth: 1,
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 10,
-    marginTop: 10, // Adiciona marginTop
+    marginTop: 10,
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    color: "black",
   },
   button: {
-    backgroundColor: "blue",
+    backgroundColor: "white",
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5,
     marginTop: 10,
   },
   buttonText: {
-    color: "white",
+    color: "black",
     fontWeight: "bold",
     textAlign: "center",
   },
   signUpText: {
     marginTop: 20,
-    color: "blue",
+    color: "white",
   },
 });
