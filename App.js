9f2162df5498b70ad8feda28d465ee1e1150fda8
Ragazzi147 <91346278+Ragazzi147@users.noreply.github.com>
2024-03-28 11:43:55 -0300

    Estrutura login

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import Card from "./src/components/PawCard";
 import users from "./assets/data/users";
+import Login from "./src/components/Login";
 
 const App = () => {
   return (
     <View style={styles.pageContainer}>
-      <Card user={users[2]} />
+      {/* <Card user={users[2]}  /> */}
+      <Login/>
     </View>
   );
 };
